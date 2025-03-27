80e234aae38e1e3545da94b6e867eb5bfd70f8d8
avdiyenkosa <avdienko.s@gmail.com>
2025-03-28 00:51:17 +0500

    fix: Change type for progress property in Command class

--- core/Command.js
+++ core/Command.js
@@ -5,34 +5,33 @@ import { progressReport } from "./ProgressReport.js";
 class Command {
   #execute;
   #undo;
-
+  #progress;
   constructor(target, operation, execute, undo) {
     this.id = randomUUID();
-    this.timestamp = this.#localTime()
+    this.timestamp = this.#localTime();
     this.target = target;
     this.operation = operation;
-    this.progress = progressReport;
+    this.#progress = progressReport;
     this.#execute = execute;
     this.#undo = undo;
   }
 
   #localTime() {
-    return Date.now()
+    return Date.now();
   }
 
   async execute() {
     try {
-      return await this.#execute(this, this.progress);
+      return await this.#execute(this, this.#progress);
     } catch (error) {
       const commandError = CommandError.ExecutionFailed(this, error);
-      commandError.log();
       throw commandError;
     }
   }
 
   async undo(cmd) {
     try {
-      return await this.#undo(cmd, this.progress);
+      return await this.#undo(cmd, this.#progress);
     } catch (error) {
       const commandError = CommandError.ExecutionFailed(this, error);
       commandError.log();
